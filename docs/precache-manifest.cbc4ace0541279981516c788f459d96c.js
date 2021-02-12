41b4c0e9a1f1363bfd3f06f886fc104ab7408027
nutrino-ballistica <77137444+nutrino-ballistica@users.noreply.github.com>
2021-02-11 21:52:46 -0500

    Release icons
    Release icons and start using docs so we can have a simpler development process.
    
    Signed-off-by: nutrino-ballistica <77137444+nutrino-ballistica@users.noreply.github.com>

---
+++ docs/precache-manifest.cbc4ace0541279981516c788f459d96c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2018c84a580010ea217",
+    "url": "/st-martin-of-tours/css/app.0aad000e.css"
+  },
+  {
+    "revision": "d769e542aeeb5d091198153de74471f7",
+    "url": "/st-martin-of-tours/favicon-new.ico"
+  },
+  {
+    "revision": "4c54077e5a3bc7cc02c3963ef5302d7e",
+    "url": "/st-martin-of-tours/favicon-new2.ico"
+  },
+  {
+    "revision": "a8385c8fa7959fdf4dc4c5c9e71e4a5d",
+    "url": "/st-martin-of-tours/img/background.a8385c8f.jpg"
+  },
+  {
+    "revision": "ea311e543ce5115f435efe8d1a49a9d3",
+    "url": "/st-martin-of-tours/img/logo.ea311e54.png"
+  },
+  {
+    "revision": "85ad1b7c18eec8575fc4195cda47d49d",
+    "url": "/st-martin-of-tours/index.html"
+  },
+  {
+    "revision": "d2018c84a580010ea217",
+    "url": "/st-martin-of-tours/js/app.2c060627.js"
+  },
+  {
+    "revision": "bb43e225e981e06819b9",
+    "url": "/st-martin-of-tours/js/chunk-vendors.93b2d32f.js"
+  },
+  {
+    "revision": "3cfa9d9c866698a28112",
+    "url": "/st-martin-of-tours/js/church.afe66646.js"
+  },
+  {
+    "revision": "5c73fe4632df2aa7cb23",
+    "url": "/st-martin-of-tours/js/contact.17d1fbac.js"
+  },
+  {
+    "revision": "e064d15f6e877fbc5baa",
+    "url": "/st-martin-of-tours/js/mission.7a2070b4.js"
+  },
+  {
+    "revision": "be50a874df7bb132c33c",
+    "url": "/st-martin-of-tours/js/worship.0ab29080.js"
+  },
+  {
+    "revision": "70963b76dd51ec3da913d56e325fd536",
+    "url": "/st-martin-of-tours/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/st-martin-of-tours/robots.txt"
+  }
+]);
